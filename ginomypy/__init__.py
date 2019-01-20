89a886ba95ac7e8157595be9f9857b9fd71783b2
Bryan Forbes <bryan@reigndropsfall.net>
2019-01-19 21:41:04 -0600

    Add basic plugin for Column

---
+++ ginomypy/__init__.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING, Optional, Callable
+from mypy.plugin import Plugin, MethodContext
+from mypy.types import Type
+from sqlmypy import column_hook
+
+if TYPE_CHECKING:
+    from typing_extensions import Final  # noqa
+
+COLUMN_NAME = 'sqlalchemy.sql.schema.Column'  # type: Final
+
+
+class BasicGinoPlugin(Plugin):
+    def get_method_hook(
+        self, fullname: str
+    ) -> Optional[Callable[[MethodContext], Type]]:
+        if fullname == COLUMN_NAME:
+            return column_hook
+        return None
+
+
+def plugin(version):
+    return BasicGinoPlugin
