df526773cbc0581a2ba1e2109114530bbcb1cda1
Bryan Forbes <bryan@reigndropsfall.net>
2019-03-04 14:55:13 -0600

    Taking a stab at making Model property return a class in the plugin

--- ginomypy/hooks.py
+++ ginomypy/hooks.py
@@ -48,6 +48,43 @@ def declarative_base_hook(ctx: DynamicClassDefContext) -> None:
     set_declarative(info)
 
 
+def gino_base_hook(ctx: DynamicClassDefContext) -> None:
+    cls_bases: List[Instance] = []
+
+    model_classes_arg: Optional[Union[TypeInfo, Expression]] = None
+    if 'model_classes' in ctx.call.arg_names:
+        model_classes_arg = ctx.call.args[ctx.call.arg_names.index('model_classes')]
+    elif len(ctx.call.args) > 1:
+        model_classes_arg = ctx.call.args[1]
+    else:
+        model_classes_arg = lookup_type_info(ctx.api, 'gino.declarative.Model')
+
+    if model_classes_arg is not None:
+        if isinstance(model_classes_arg, TupleExpr):
+            items: List[Union[Expression, TypeInfo]] = [
+                item for item in model_classes_arg.items
+            ]
+        else:
+            items = [model_classes_arg]
+
+        for item in items:
+            base: Optional[Union[Instance, TupleType]] = None
+            if isinstance(item, RefExpr) and isinstance(item.node, TypeInfo):
+                base = fill_typevars_with_any(item.node)
+            elif isinstance(item, TypeInfo):
+                base = fill_typevars_with_any(item)
+
+            if isinstance(base, Instance):
+                cls_bases.append(base)
+
+    model_type_info = lookup_type_info(ctx.api, 'gino.declarative.ModelType')
+    info = create_dynamic_class(
+        ctx, cls_bases, metaclass=model_type_info, name=f'{ctx.name}.Model'
+    )
+
+    set_declarative(info)
+
+
 def model_hook(ctx: Union[FunctionContext, MethodContext]) -> Type:
     model = get_model_from_ctx(ctx)
 
--- ginomypy/plugin.py
+++ ginomypy/plugin.py
@@ -1,9 +1,20 @@
 from typing import Optional, Callable, TypeVar
-from mypy.plugin import Plugin, FunctionContext, MethodContext, DynamicClassDefContext
+from mypy.plugin import (
+    Plugin,
+    FunctionContext,
+    MethodContext,
+    DynamicClassDefContext,
+    AnalyzeTypeContext,
+)
 from mypy.types import Type
 from sqlmypy import column_hook  # type: ignore
 
-from .hooks import model_hook, crud_model_values_hook, declarative_base_hook
+from .hooks import (
+    model_hook,
+    crud_model_values_hook,
+    declarative_base_hook,
+    gino_base_hook,
+)
 from .names import COLUMN_NAME, DECLARATIVE_BASE_NAME, VALUES_NAMES
 from .utils import is_declarative, lookup_type_info
 
@@ -43,5 +54,13 @@ class GinoPlugin(Plugin):
     def get_dynamic_class_hook(self, fullname: str) -> CB[DynamicClassDefContext]:
         if fullname == DECLARATIVE_BASE_NAME:
             return declarative_base_hook
+        if fullname == 'gino.api.Gino':
+            return gino_base_hook
 
+        return None
+
+    def get_type_analyze_hook(
+        self, fullname: str
+    ) -> Optional[Callable[[AnalyzeTypeContext], Type]]:
+        print(fullname)
         return None
